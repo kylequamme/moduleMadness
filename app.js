947c051bce5fc967575dfb62d435677aeed62791
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-07-06 09:50:20 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+// Import modules
+var mod3 = require('./mod3.js');
+
+console.log(mod3.text + mod3.ranNumToUSD);
---
+++ mod3.js
@@ -0,0 +1,8 @@
+//Import modules
+var mod1 = require('./mod1.js');
+var mod2 = require('./mod2.js');
+
+module.exports = {
+  ranNumToUSD: mod2(mod1(100, 1000000)),
+  text: 'Account balance: \n'
+}
